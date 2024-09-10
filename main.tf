54fc983fd71d95825865207ad5bfaf9214846682
Roger Tadeu Barreto Santos <roger.tadeu@gmail.com>
2024-09-10 20:32:51 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,9 +38,11 @@ module "project-services" {
     "storage.googleapis.com",         # Cloud Storage
     "documentai.googleapis.com",      # Document AI
     "eventarc.googleapis.com",        # Eventarc
+    "pubsub.googleapis.com",          # Pub/Sub (required for Eventarc transport)
   ]
 }
 
+# Generate a random ID
 resource "random_id" "id" {
   byte_length = 4
 }
@@ -48,8 +50,8 @@ resource "random_id" "id" {
 # Document AI Processor
 resource "google_document_ai_processor" "document_ai_ocr" {
   display_name = "Document AI OCR Processor"
-  type         = "OCR_PROCESSOR"
-  location     = "us"
+  type         = var.document_ai_processor_type  # Use variable for processor type (OCR by default)
+  location     = var.location                    # Use variable for location
 }
 
 # Enable API for Document AI
@@ -60,8 +62,8 @@ resource "google_project_service" "documentai" {
 
 # Vertex AI Featurestore for Vector Embeddings
 resource "google_vertex_ai_featurestore" "featurestore" {
-  display_name = "Vector Embeddings"
-  region       = var.region
+  display_name = var.vertex_ai_featurestore_name  # Use variable for Featurestore name
+  region       = var.region                       # Use variable for region
 }
 
 resource "google_vertex_ai_featurestore_entitytype" "entity_type" {
@@ -80,4 +82,70 @@ resource "google_project_service" "aiplatform" {
 resource "google_project_service" "eventarc" {
   service = "eventarc.googleapis.com"
   project = var.project_id
+}
+
+# Pub/Sub topic for Eventarc
+resource "google_pubsub_topic" "upload_topic" {
+  name = var.eventarc_trigger_name
+}
+
+# Eventarc Trigger for Cloud Storage
+resource "google_eventarc_trigger" "document_upload_trigger" {
+  name     = var.eventarc_trigger_name
+  location = var.region
+
+  matching_criteria {
+    attribute = "type"
+    value     = "google.cloud.storage.object.v1.finalized"
+  }
+
+  matching_criteria {
+    attribute = "bucket"
+    value     = google_storage_bucket.staging_bucket.name
+  }
+
+  transport {
+    pubsub {
+      topic = google_pubsub_topic.upload_topic.id
+    }
+  }
+
+  destination {
+    cloud_function {
+      uri = google_cloudfunctions_function.document_processor.https_trigger_url
+    }
+  }
+}
+
+# Cloud Function to process uploaded documents
+resource "google_cloudfunctions_function" "document_processor" {
+  name        = "document-processor"
+  runtime     = "python310"
+  entry_point = "process_document"
+  region      = var.region
+
+  source_archive_bucket = google_storage_bucket.staging_bucket.name
+  source_archive_object = "function_source.zip"
+
+  https_trigger {}
+
+  environment_variables = {
+    BUCKET_NAME  = google_storage_bucket.staging_bucket.name
+    PROCESSOR_ID = google_document_ai_processor.document_ai_ocr.name
+  }
+
+  service_account_email = google_service_account.cloud_function_sa.email
+  timeout               = 60
+}
+
+# Service account for Cloud Functions
+resource "google_service_account" "cloud_function_sa" {
+  account_id   = "cloud-function-service-account"
+  display_name = "Cloud Function Service Account"
+}
+
+# IAM roles for Cloud Function service account
+resource "google_project_iam_member" "cloud_function_iam" {
+  role   = "roles/cloudfunctions.invoker"
+  member = "serviceAccount:${google_service_account.cloud_function_sa.email}"
 }
