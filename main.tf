bb96c93077de7d798e665fc1d13895d520a7b77b
Roger Tadeu Barreto Santos <roger.tadeu@gmail.com>
2024-09-10 20:42:06 -0300

    Revert "feat: document ai ocr processing trigger"
    This reverts commit aec01bd65f1acb204d079c385759a77592402dc9.

--- main.tf
+++ main.tf
@@ -23,61 +23,22 @@ module "project-services" {
   enable_apis = var.enable_apis
 
   activate_apis = [
-    "aiplatform.googleapis.com",      # Vertex AI
+    "aiplatform.googleapis.com",
     "artifactregistry.googleapis.com",
     "cloudapis.googleapis.com",
     "cloudbuild.googleapis.com",
     "compute.googleapis.com",
     "config.googleapis.com",
     "iam.googleapis.com",
-    "run.googleapis.com",             # Cloud Run
+    "run.googleapis.com",
     "secretmanager.googleapis.com",
     "serviceusage.googleapis.com",
-    "sqladmin.googleapis.com",        # Cloud SQL
+    "sqladmin.googleapis.com",
     "storage-api.googleapis.com",
-    "storage.googleapis.com",         # Cloud Storage
-    "documentai.googleapis.com",      # Document AI
-    "eventarc.googleapis.com",        # Eventarc
+    "storage.googleapis.com",
   ]
 }
 
 resource "random_id" "id" {
   byte_length = 4
-}
-
-# Document AI Processor
-resource "google_document_ai_processor" "document_ai_ocr" {
-  display_name = "Document AI OCR Processor"
-  type         = "OCR_PROCESSOR"
-  location     = "us"
-}
-
-# Enable API for Document AI
-resource "google_project_service" "documentai" {
-  service = "documentai.googleapis.com"
-  project = var.project_id
-}
-
-# Vertex AI Featurestore for Vector Embeddings
-resource "google_vertex_ai_featurestore" "featurestore" {
-  display_name = "Vector Embeddings"
-  region       = var.region
-}
-
-resource "google_vertex_ai_featurestore_entitytype" "entity_type" {
-  featurestore = google_vertex_ai_featurestore.featurestore.name
-  entitytype_id = "vector-embedding"
-  description   = "Embeddings for processed documents"
-}
-
-# Enable Vertex AI Platform APIs
-resource "google_project_service" "aiplatform" {
-  service = "aiplatform.googleapis.com"
-  project = var.project_id
-}
-
-# Enable Eventarc API
-resource "google_project_service" "eventarc" {
-  service = "eventarc.googleapis.com"
-  project = var.project_id
 }
